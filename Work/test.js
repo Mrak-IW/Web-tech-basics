688bde8a2f097a7815c04afb4803b54145f18c87
Mrak-IW <mrak.iw@gmail.com>
2015-11-30 17:09:13 +0200

    Устранены баги теста

--- Work/test.js
+++ Work/test.js
@@ -5,15 +5,40 @@ function testResult() {
 	var elems = document.forms[0].elements;
 	var sum = 0
 	var max = 0;
+	var checkGroups = [];
 	for (var i = 0; i < elems.length; i++) {
-		if ((elems[i].type == "radio" || elems[i].type == "checkbox") && elems[i].value == 1) {
-			max++;
-			if (elems[i].checked) {
-				sum++;
-			}
+		switch (elems[i].type)
+		{
+			case "checkbox":
+				if (checkGroups[elems[i].name] === undefined) {
+					checkGroups[elems[i].name] = true;
+					max++;
+				}
+				if (((elems[i].value == 1) || elems[i].checked) && !((elems[i].value == 1) && elems[i].checked)) {
+					checkGroups[elems[i].name] = false;
+				}
+				break;
+			case "radio":
+				if(elems[i].value == 1) {
+					if (elems[i].checked) {
+						sum++;
+					}
+					max++;
+				}
+				break;
+		}
+		
+	}
+	
+	console.log(sum);
+	for (var i in checkGroups) {
+		if (checkGroups[i]) {
+			sum++;
 		}
 	}
 	
+	console.log(checkGroups);
+	console.log(sum);
 	resElem.innerHTML = "Ваш результат: " + sum + "/" + max;
 	pointOfInsert.appendChild(resElem);
 }
