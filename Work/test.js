23e1d9f7d96a00802ac1c0926604949d72ea76e5
Mrak-IW <mrak.iw@gmail.com>
2015-11-30 21:42:50 +0200

    Убраны лишние выводы в лог. Обработка радиобаттонов и чекбоксов сведена в одну логику.

--- Work/test.js
+++ Work/test.js
@@ -6,39 +6,25 @@ function testResult() {
 	var sum = 0
 	var max = 0;
 	var checkGroups = [];
+	
 	for (var i = 0; i < elems.length; i++) {
-		switch (elems[i].type)
-		{
-			case "checkbox":
-				if (checkGroups[elems[i].name] === undefined) {
-					checkGroups[elems[i].name] = true;
-					max++;
-				}
-				if (((elems[i].value == 1) || elems[i].checked) && !((elems[i].value == 1) && elems[i].checked)) {
-					checkGroups[elems[i].name] = false;
-				}
-				break;
-			case "radio":
-				if(elems[i].value == 1) {
-					if (elems[i].checked) {
-						sum++;
-					}
-					max++;
-				}
-				break;
+		if ((elems[i].type == "checkbox") || (elems[i].type == "radio")) {
+			if (checkGroups[elems[i].name] === undefined) {
+				checkGroups[elems[i].name] = true;
+				max++;
+			}
+			if (((elems[i].value == 1) || elems[i].checked) && !((elems[i].value == 1) && elems[i].checked)) {
+				checkGroups[elems[i].name] = false;
+			}
 		}
-		
 	}
 	
-	console.log(sum);
 	for (var i in checkGroups) {
 		if (checkGroups[i]) {
 			sum++;
 		}
 	}
 	
-	console.log(checkGroups);
-	console.log(sum);
 	resElem.innerHTML = "Ваш результат: " + sum + "/" + max;
 	pointOfInsert.appendChild(resElem);
 }
