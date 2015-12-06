7875d94f36e5999b1823f0e6175e1103357e08dd
Mrak-IW <mrak.iw@gmail.com>
2015-12-07 00:20:44 +0200

    Функционал теста переведен на JQuery

---
+++ Work/testJQ.js
@@ -0,0 +1,62 @@
+var resElem = $("<p></p>");
+
+function testResult() {
+	var sum = 0
+	var max = 0;
+	var checkGroups = [];
+	var mustChecked;
+	
+	$(":checkbox").each(function(index, elem){
+		var el = $(elem);
+		if (checkGroups[el.attr("name")] === undefined) {
+			checkGroups[el.attr("name")] = {};
+			checkGroups[el.attr("name")].right = 0;
+			checkGroups[el.attr("name")].countRight = 0;
+			max++;
+		}
+		
+		mustChecked = decode(+el.attr("value"), +el.attr("code"));
+		
+		if (mustChecked) {
+			checkGroups[el.attr("name")].countRight++;
+			if (el.prop("checked")) {
+				checkGroups[el.attr("name")].right++;
+			}
+		}
+		
+		if (!mustChecked && el.prop("checked")) {
+			checkGroups[el.attr("name")].right--;
+		}
+	});
+	
+	$(":radio").each(function(index, elem){
+		var el = $(elem);
+		
+		mustChecked = decode(+el.attr("value"), +el.attr("code"));
+		
+		if(mustChecked) {
+			if (el.prop("checked")) {
+				sum++;
+			}
+			max++;
+		}
+	});
+	
+	
+	for (var i in checkGroups) {
+		if (checkGroups[i].right > 0) {
+			sum += checkGroups[i].right / checkGroups[i].countRight;
+		}
+	}
+	
+	resElem.html("Ваш результат: " + sum + "/" + max);
+	$(".body").append(resElem);
+}
+
+function decode(value, code) {
+	var pow2;
+	
+	pow2 = Math.pow(2, value);
+	
+	return ((code - code % pow2) / pow2) % 2 == 1;
+}
